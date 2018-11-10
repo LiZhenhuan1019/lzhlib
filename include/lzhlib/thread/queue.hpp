553186db6e3152dde15f30510b6e7e4fe8dab9db
Li Zhenhuan <lizhenhuan1019@qq.com>
2018-11-10 21:21:50 +0800

    fix bug of thread::queue::push and add thread::queue::wait_for_and_pop

--- include/lzhlib/thread/queue.hpp
+++ include/lzhlib/thread/queue.hpp
@@ -2,6 +2,7 @@
 #define LZHLIB_QUEUE_HPP
 #include <type_traits>
 #include <optional>
+#include <chrono>
 #include <memory>
 #include <mutex>
 #include <condition_variable>
@@ -24,6 +25,23 @@ namespace lzhlib
             queue(queue const &) = delete;
             queue const &operator=(queue const &) = delete;
 
+            template <typename Duration, typename = std::enable_if_t<std::is_nothrow_move_constructible_v<T>>>
+            std::optional<T> wait_for_and_pop(Duration duration)
+            {
+                std::unique_ptr<node> popped = wait_pop_head_for(duration);
+                return popped ? std::move(popped->data) : std::nullopt;
+            }
+            template <typename Duration, typename = std::enable_if_t<std::is_nothrow_move_assignable_v<T>>>
+            bool wait_for_and_pop(T &out_value, Duration duration)
+            {
+                std::unique_ptr<node> popped = wait_pop_head_for(duration);
+                if (popped)
+                {
+                    out_value = std::move(popped->data.value());
+                    return true;
+                }
+                return false;
+            }
             template <typename = std::enable_if_t<std::is_nothrow_move_constructible_v<T>>>
             T wait_and_pop()
             {
@@ -38,7 +56,7 @@ namespace lzhlib
             std::optional<T> try_pop()
             {
                 std::unique_ptr<node> popped = try_pop_head();
-                return popped ? popped->data : std::nullopt;
+                return popped ? std::move(popped->data) : std::nullopt;
             }
             template <typename = std::enable_if_t<std::is_nothrow_move_assignable_v<T>>>
             bool try_pop(T &out_value)
@@ -55,9 +73,9 @@ namespace lzhlib
             {
                 std::unique_ptr<node> new_node = std::make_unique<node>();
                 node *const new_tail = new_node.get();
-                tail->data = std::move(value);
                 {
                     std::lock_guard<std::mutex> tail_lock(tail_mutex);
+                    tail->data = std::move(value);
                     tail->next = std::move(new_node);
                     tail = new_tail;
                 }
@@ -80,6 +98,17 @@ namespace lzhlib
                 std::unique_ptr<node> popped_head = std::move(head);
                 head = std::move(popped_head->next);
                 return popped_head;
+            }
+            template <typename Duration>
+            std::unique_ptr<node> wait_pop_head_for(Duration duration)
+            {
+                auto now = std::chrono::system_clock::now();
+                std::unique_lock<std::mutex> head_lock(head_mutex);
+                using namespace std::chrono_literals;
+                if (has_new_element.wait_until(head_lock, now + duration, [this] { return head.get() != get_tail(); }))
+                    return pop_head();
+                else
+                    return nullptr;
             }
             std::unique_ptr<node> wait_pop_head()
             {
